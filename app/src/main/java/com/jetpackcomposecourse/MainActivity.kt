abfd85206a6b5457241dcc9865760b7fa937327d
Lucas-Zacarias <lucas21111999@gmail.com>
2023-12-20 12:06:34 -0300

    greeting card practice

--- app/src/main/java/com/jetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/jetpackcomposecourse/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jetpackcomposecourse.ui.theme.JetpackComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +22,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.Cyan
                 ) {
-                    Greeting("Android")
+                    Greeting("Lucas")
                 }
             }
         }
@@ -32,8 +34,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hi, my name is $name!",
+        modifier = modifier.padding(24.dp)
     )
 }
 
@@ -41,6 +43,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JetpackComposeCourseTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color.Cyan
+        ) {
+            Greeting("Lucas")
+        }
     }
 }
