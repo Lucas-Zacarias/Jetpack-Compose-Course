413a6c5800f935a1748a0142b12e731d88220822
Lucas-Zacarias <lucas21111999@gmail.com>
2024-01-05 12:01:49 -0300

    first version of tip calculator

--- app/src/main/java/com/jetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/jetpackcomposecourse/MainActivity.kt
@@ -4,24 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.jetpackcomposecourse.ui.theme.BackgroundGray
+import com.jetpackcomposecourse.ui.practice.TipCalculator
 import com.jetpackcomposecourse.ui.theme.JetpackComposeCourseTheme
+import com.jetpackcomposecourse.ui.theme.TipTimeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeCourseTheme {
+            TipTimeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = BackgroundGray
+                    color = MaterialTheme.colorScheme.background
                 ) {
-
+                    TipCalculator()
                 }
             }
         }
