43985c67e4675debf4c70e2fad049d6be98c4050
Lucas-Zacarias <lucas21111999@gmail.com>
2024-01-15 10:12:47 -0300

    set data source of the app

---
+++ app/src/main/java/com/jetpackcomposecourse/data/model/StretchingList.kt
@@ -0,0 +1,38 @@
+package com.jetpackcomposecourse.data.model
+
+import com.jetpackcomposecourse.R
+
+object StretchingList {
+    val stretchingList = listOf(
+        Stretching(nameRes = R.string.ex1_title, descriptionRes = R.string.ex1_description, imageRes = R.drawable.ej1_bombeo_brazos),
+        Stretching(nameRes = R.string.ex2_title, descriptionRes = R.string.ex2_description, imageRes = R.drawable.ej2_molinos),
+        Stretching(nameRes = R.string.ex3_title, descriptionRes = R.string.ex3_description, imageRes = R.drawable.ej3_estiramiento_pecho),
+        Stretching(nameRes = R.string.ex4_title, descriptionRes = R.string.ex4_description, imageRes = R.drawable.ej4_estiramiento_isquiotibiales_hombros),
+        Stretching(nameRes = R.string.ex5_title, descriptionRes = R.string.ex5_description, imageRes = R.drawable.ej5_estiramiento_cuadriceps),
+        Stretching(nameRes = R.string.ex6_title, descriptionRes = R.string.ex6_description, imageRes = R.drawable.ej6_estiramiento_tendones),
+        Stretching(nameRes = R.string.ex7_title, descriptionRes = R.string.ex7_description, imageRes = R.drawable.ej7_caminar_talones),
+        Stretching(nameRes = R.string.ex8_title, descriptionRes = R.string.ex8_description, imageRes = R.drawable.ej8_estocada_lateral),
+        Stretching(nameRes = R.string.ex9_title, descriptionRes = R.string.ex9_description, imageRes = R.drawable.ej9_perro_pajaro),
+        Stretching(nameRes = R.string.ex10_title, descriptionRes = R.string.ex10_description, imageRes = R.drawable.ej10_perro_boca_abajo),
+        Stretching(nameRes = R.string.ex11_title, descriptionRes = R.string.ex11_description, imageRes = R.drawable.ej11_postura_nino),
+        Stretching(nameRes = R.string.ex12_title, descriptionRes = R.string.ex12_description, imageRes = R.drawable.ej12_estiramiento_romboide),
+        Stretching(nameRes = R.string.ex13_title, descriptionRes = R.string.ex13_description, imageRes = R.drawable.ej13_pose_nino_extendida),
+        Stretching(nameRes = R.string.ex14_title, descriptionRes = R.string.ex14_description, imageRes = R.drawable.ej14_estiramiento_pectoral),
+        Stretching(nameRes = R.string.ex15_title, descriptionRes = R.string.ex15_description, imageRes = R.drawable.ej15_pose_nino_extensiones_laterales),
+        Stretching(nameRes = R.string.ex16_title, descriptionRes = R.string.ex16_description, imageRes = R.drawable.ej16_puente_suelo),
+        Stretching(nameRes = R.string.ex17_title, descriptionRes = R.string.ex17_description, imageRes = R.drawable.ej17_estiramiento_espalda_balanceo),
+        Stretching(nameRes = R.string.ex18_title, descriptionRes = R.string.ex18_description, imageRes = R.drawable.ej18_postura_asado),
+        Stretching(nameRes = R.string.ex19_title, descriptionRes = R.string.ex19_description, imageRes = R.drawable.ej19_inclinaciones_pelvis),
+        Stretching(nameRes = R.string.ex20_title, descriptionRes = R.string.ex20_description, imageRes = R.drawable.ej20_elevaciones_pierna_derecha),
+        Stretching(nameRes = R.string.ex21_title, descriptionRes = R.string.ex21_description, imageRes = R.drawable.ej21_elevaciones_pierna_izquierda),
+        Stretching(nameRes = R.string.ex22_title, descriptionRes = R.string.ex22_description, imageRes = R.drawable.ej22_postura_media_cobra),
+        Stretching(nameRes = R.string.ex23_title, descriptionRes = R.string.ex23_description, imageRes = R.drawable.ej23_yta),
+        Stretching(nameRes = R.string.ex24_title, descriptionRes = R.string.ex24_description, imageRes = R.drawable.ej24_postura_arco),
+        Stretching(nameRes = R.string.ex25_title, descriptionRes = R.string.ex25_description, imageRes = R.drawable.ej25_inclinaciones_adelante),
+        Stretching(nameRes = R.string.ex26_title, descriptionRes = R.string.ex26_description, imageRes = R.drawable.ej26_estiramiento_piernas_centro),
+        Stretching(nameRes = R.string.ex27_title, descriptionRes = R.string.ex27_description, imageRes = R.drawable.ej27_estiramiento_pierna_derecha),
+        Stretching(nameRes = R.string.ex28_title, descriptionRes = R.string.ex28_description, imageRes = R.drawable.ej28_estiramiento_pierna_izquierda),
+        Stretching(nameRes = R.string.ex29_title, descriptionRes = R.string.ex29_description, imageRes = R.drawable.ej29_estiramiento_ambas_piernas),
+        Stretching(nameRes = R.string.ex30_title, descriptionRes = R.string.ex30_description, imageRes = R.drawable.ej30_cuerpo_muerto)
+    )
+}
---
+++ app/src/main/java/com/jetpackcomposecourse/ui/practice/30DaysApp.kt
@@ -0,0 +1,8 @@
+package com.jetpackcomposecourse.ui.practice
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DaysApp() {
+
+}
