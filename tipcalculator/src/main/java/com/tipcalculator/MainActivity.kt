4fec9b43ee7e77553ec42417c03ccee101522a97
Lucas-Zacarias <lucas21111999@gmail.com>
2024-02-27 14:32:23 -0300

    tip calculator empty module setup

---
+++ tipcalculator/src/main/java/com/tipcalculator/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.tipcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tipcalculator.ui.theme.TipCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipCalculatorTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TipCalculatorPreview() {
+    TipCalculatorTheme {
+
+    }
+}
