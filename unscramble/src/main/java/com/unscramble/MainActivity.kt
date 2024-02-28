cb18c4d22a4500dc0260d1c348399faf9588bd91
Lucas-Zacarias <lucas21111999@gmail.com>
2024-02-28 14:23:48 -0300

    unscramble app empty module setup

---
+++ unscramble/src/main/java/com/unscramble/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.unscramble
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.unscramble.ui.theme.UnscrambleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnscrambleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UnscramblePreview() {
+    UnscrambleTheme {
+
+    }
+}
