66533841af6294113c2b59404a3f1bf69f5a6729
Lucas-Zacarias <lucas21111999@gmail.com>
2024-02-28 15:37:14 -0300

    cupcake app empty module setup

---
+++ cupcake/src/main/java/com/cupcake/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.cupcake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cupcake.ui.theme.CupcakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CupcakeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CupcakePreview() {
+    CupcakeTheme {
+
+    }
+}
