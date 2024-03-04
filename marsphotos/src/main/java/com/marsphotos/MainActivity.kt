1f7603df8571d6352cb21cfa8401fbc80fcaebe3
Lucas-Zacarias <lucas21111999@gmail.com>
2024-03-04 16:17:33 -0300

    mars photos app starter code setup

---
+++ marsphotos/src/main/java/com/marsphotos/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.marsphotos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.marsphotos.ui.MarsPhotosApp
+import com.marsphotos.ui.theme.MarsPhotosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MarsPhotosTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MarsPhotosApp()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MarsPhotosPreview() {
+    MarsPhotosTheme {
+        MarsPhotosApp()
+    }
+}
