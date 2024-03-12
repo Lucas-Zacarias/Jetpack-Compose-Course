426c8119b318e76157920b02f76aa5ee1b0a3c82
Lucas Zacarias <lucas21111999@gmail.com>
2024-03-12 14:15:46 -0300

    base code implementation copied from source code

--- sqlbasics/src/main/java/com/sqlbasics/MainActivity.kt
+++ sqlbasics/src/main/java/com/sqlbasics/MainActivity.kt
@@ -3,22 +3,35 @@ package com.sqlbasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.sqlbasics.ui.theme.SqlBasicsTheme
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        GlobalScope.launch {
+            AppDatabase.getDatabase(applicationContext).emailDao().getAll()
+        }
         setContent {
             SqlBasicsTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("The database is ready!")
+                    }
                 }
             }
         }
