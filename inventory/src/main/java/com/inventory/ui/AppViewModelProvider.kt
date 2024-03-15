4fda9eab6330c3f70f5ee51b179b7e2f5dd3da9f
Lucas-Zacarias <lucas21111999@gmail.com>
2024-03-15 09:24:23 -0300

    displaying inventory items using flow and stateflow

--- inventory/src/main/java/com/inventory/ui/AppViewModelProvider.kt
+++ inventory/src/main/java/com/inventory/ui/AppViewModelProvider.kt
@@ -38,7 +38,7 @@ object AppViewModelProvider {
 
         // Initializer for HomeViewModel
         initializer {
-            HomeViewModel()
+            HomeViewModel(inventoryApplication().container.itemsRepository)
         }
     }
 }
