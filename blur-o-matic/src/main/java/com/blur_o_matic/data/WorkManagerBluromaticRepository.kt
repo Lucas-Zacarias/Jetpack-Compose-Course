96217626539aa48f1e958407dc0762f8da4e69e5
lucaszacarias <lucas.zacarias@itrocksweb.com>
2024-11-07 20:09:33 -0300

    enqueue workers

--- blur-o-matic/src/main/java/com/blur_o_matic/data/WorkManagerBluromaticRepository.kt
+++ blur-o-matic/src/main/java/com/blur_o_matic/data/WorkManagerBluromaticRepository.kt
@@ -3,6 +3,7 @@ package com.blur_o_matic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
@@ -10,6 +11,8 @@ import com.blur_o_matic.KEY_BLUR_LEVEL
 import com.blur_o_matic.KEY_IMAGE_URI
 import com.blur_o_matic.getImageUri
 import com.blur_o_matic.workers.BlurWorker
+import com.blur_o_matic.workers.CleanupWorker
+import com.blur_o_matic.workers.SaveImageToFileWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -24,14 +27,24 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
      * @param blurLevel The amount to blur the image
      */
     override fun applyBlur(blurLevel: Int) {
+        var continuation = workManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
         // Create WorkRequest to blur the image
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
 
         blurBuilder
             .setInputData(createInputDataForWorkRequest(blurLevel = blurLevel, imageUri = imageUri))
 
-        // Start the work
-        workManager.enqueue(blurBuilder.build())
+        continuation = continuation.then(blurBuilder.build())
+
+        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+
+        continuation = continuation.then(save.build())
+
+        // Actually start the work
+        continuation.enqueue()
+
+//        // Start the work
+//        workManager.enqueue(blurBuilder.build())
     }
 
     /**
