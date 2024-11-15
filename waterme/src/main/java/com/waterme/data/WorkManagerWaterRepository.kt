9c54c8823ca3b4047428e7dd96e9ea726eed7e9e
lucaszacarias <lucas.zacarias@itrocksweb.com>
2024-11-15 20:28:20 -0300

    Schedule notification using WorkManager

--- waterme/src/main/java/com/waterme/data/WorkManagerWaterRepository.kt
+++ waterme/src/main/java/com/waterme/data/WorkManagerWaterRepository.kt
@@ -1,8 +1,13 @@
 package com.waterme.data
 
 import android.content.Context
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.waterme.model.Plant
+import com.waterme.worker.WaterReminderWorker
+import com.waterme.worker.WaterReminderWorker.Companion.nameKey
 import java.util.concurrent.TimeUnit
 
 class WorkManagerWaterRepository(context: Context) : WaterRepository {
@@ -11,5 +16,16 @@ class WorkManagerWaterRepository(context: Context) : WaterRepository {
     override val plants: List<Plant>
         get() = DataSource.plants
 
-    override fun scheduleReminder(duration: Long, unit: TimeUnit, plantName: String) {}
+    override fun scheduleReminder(duration: Long, unit: TimeUnit, plantName: String) {
+        val workName = "$plantName $duration $unit"
+        val waterReminderWorker = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInputData(workDataOf(nameKey to plantName))
+            .build()
+
+        workManager.enqueueUniqueWork(
+            workName,
+            ExistingWorkPolicy.REPLACE,
+            waterReminderWorker
+        )
+    }
 }
