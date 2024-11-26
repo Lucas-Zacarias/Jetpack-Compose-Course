b8202b3497608cae2290743988e7b9bb3198d9f9
lucaszacarias <lucas.zacarias@itrocksweb.com>
2024-11-26 20:08:54 -0300

    missing dependency

--- juicetraker/build.gradle.kts
+++ juicetraker/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.material:material:1.11.0")
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.accompanist:accompanist-themeadapter-material3:0.28.0")
